2dad6de595f4127606b0c83e1cc4e2c4043e940c
Michael Burns <michael@mirwin.net>
2017-04-21 05:05:45 -0700

    lint

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,32 +12,31 @@ end
   package p
 end
 
-template "/etc/rackspace-monitoring-poller.cfg" do
-    owner node['poller']['owner']
-    group node['poller']['group']
-    mode 0o0644
-    variables(
-        :monitoring_token => node['poller']['monitoring_token'],
-        :monitoring_id => node['poller']['monitoring_id'],
-        :monitoring_private_zones => node['poller']['monitoring_private_zones']
-    )
-    source 'rackspace-monitoring-poller.cfg.erb'
-    notifies :restart, 'service[rackspace-monitoring-poller]'
+template '/etc/rackspace-monitoring-poller.cfg' do
+  owner node['poller']['owner']
+  group node['poller']['group']
+  mode 0o0644
+  variables(
+    monitoring_token: node['poller']['monitoring_token'],
+    monitoring_id: node['poller']['monitoring_id'],
+    monitoring_private_zones: node['poller']['monitoring_private_zones']
+  )
+  source 'rackspace-monitoring-poller.cfg.erb'
+  notifies :restart, 'service[rackspace-monitoring-poller]'
 end
 
-template "/etc/default/rackspace-monitoring-poller" do
-    owner 'root'
-    group 'root'
-    mode 0o0644
-    variables(
-        :enabled => node['poller']['enabled']
-    )
-    source 'rackspace-monitoring-poller.erb'
-    notifies :restart, 'service[rackspace-monitoring-poller]'
+template '/etc/default/rackspace-monitoring-poller' do
+  owner 'root'
+  group 'root'
+  mode 0o0644
+  variables(
+    enabled: node['poller']['enabled']
+  )
+  source 'rackspace-monitoring-poller.erb'
+  notifies :restart, 'service[rackspace-monitoring-poller]'
 end
 
 service 'rackspace-monitoring-poller' do
   supports restart: true, status: true
   action [:enable, :start]
 end
-
